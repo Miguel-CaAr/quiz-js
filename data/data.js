700a9d832614ce7e7cb45aa38f7c291610499abc
Miguel-CaAr <castroarredondo.hector@gmail.com>
2023-09-17 18:53:15 -0700

    Lista de preguntas, opciones y respuestas.

---
+++ data/data.js
@@ -0,0 +1,24 @@
+export const data = [
+  {
+    question:
+      "Cual de las siguientes opciones no es un lenguaje de programacion?",
+    choices: ["Python", "Ruby", "JavaScript", "CSS"],
+    answer: "CSS",
+  },
+  {
+    question:
+      "Cual no es una caracteristica del paradigma de programacion orientado a objetos?",
+    choices: ["Poliformismo", "Herencia", "Abstraccion", "Instrasendencia"],
+    answer: "Instrasendencia",
+  },
+  {
+    question: "Cual es el significado de las siglas DOM",
+    choices: [
+      "Object domain modeling",
+      "Document object model",
+      "Data Overload Manager",
+      "Dinosaur Observation Module",
+    ],
+    answer: "Document object model",
+  },
+];
